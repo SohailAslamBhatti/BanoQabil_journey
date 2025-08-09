ceaecee9b7f87466de49b58bd8c90cb45e72e205
Sohail Aslam Bhatti <sohailaslambhatti@outlook.com>
2025-08-09 13:24:19 +0500

    Add files via upload

---
+++ Assignment_4a/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import BulbToggle from "./components/bulbToggle";
+// import './components/BulbToggle.css';
+
+
+export default function App() {
+  return <BulbToggle />;
+}
+
