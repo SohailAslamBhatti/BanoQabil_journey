74f181bf423fbee6005efdec9267d3ae7ac02c0f
Sohail Aslam Bhatti <sohailaslambhatti@outlook.com>
2025-08-09 15:24:34 +0500

    Delete Assignment_4a directory

--- Assignment_4a/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import BulbToggle from "./components/bulbToggle";
-// import './components/BulbToggle.css';
-
-
-export default function App() {
-  return <BulbToggle />;
-}
-
